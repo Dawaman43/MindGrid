d96207e4e4c98fc0d1abfda04b989349ae8a463d
Dawit Worku <144016968+Dawaman43@users.noreply.github.com>
2026-02-05 22:53:04 +0300

    Design additional web pages

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -8,12 +8,14 @@ export default function Header() {
   const links = [
     { to: "/", label: "Home" },
     { to: "/dashboard", label: "Dashboard" },
+    { to: "/integrations", label: "Integrations" },
+    { to: "/analytics", label: "Analytics" },
   ] as const;
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between px-2 py-1">
-        <nav className="flex gap-4 text-lg">
+        <nav className="flex flex-wrap gap-4 text-lg">
           {links.map(({ to, label }) => {
             return (
               <Link key={to} href={to}>

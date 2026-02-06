86dc4396629372d0e6bf5c467a4627463e5c0e6a
Dawit Worku <144016968+Dawaman43@users.noreply.github.com>
2026-02-06 10:48:39 +0300

    Add core schema and routers

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,9 +1,15 @@
 import { protectedProcedure, publicProcedure, router } from "../index";
+import { gridRouter } from "./grid";
+import { projectRouter } from "./project";
+import { workspaceRouter } from "./workspace";
 
 export const appRouter = router({
   healthCheck: publicProcedure.query(() => {
     return "OK";
   }),
+  workspace: workspaceRouter,
+  project: projectRouter,
+  grid: gridRouter,
   privateData: protectedProcedure.query(({ ctx }) => {
     return {
       message: "This is private",
